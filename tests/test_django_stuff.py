f9c81256e42ef48de019669dcadb8baf90320ba4
Rami ALBASHA <ralbasha@msfourrager.com>
2024-11-18 17:30:34 -0500

    test func that gets SLC polygon based on farm lat long coordinates

---
+++ tests/test_django_stuff.py
@@ -0,0 +1,41 @@
+import unittest
+
+from holos_service import django_stuff
+
+
+class TestIdentifySlcPolygonId(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.geojson_data = django_stuff.load_slc_data()
+
+    def test_identify_slc_polygon_id_works_with_default_slc_data(self):
+        self.assertEqual(
+            851003,
+            django_stuff.identify_slc_polygon_id(
+                latitude=49.98,
+                longitude=-98.04))
+
+    def test_identify_slc_polygon_id_returns_expected_result(self):
+        self.assertEqual(
+            851003,
+            django_stuff.identify_slc_polygon_id(
+                latitude=49.98,
+                longitude=-98.04,
+                geojson_data=self.geojson_data))
+
+        self.assertEqual(
+            851003,
+            django_stuff.identify_slc_polygon_id(
+                latitude="49.98",
+                longitude="-98.04",
+                geojson_data=self.geojson_data))
+
+    def test_identify_slc_polygon_id_fails_with_missing_location_data(self):
+        self.assertRaises(
+            TypeError,
+            django_stuff.identify_slc_polygon_id,
+            dict(longitude=-98.04, geojson_data=self.geojson_data))
+
+
+if __name__ == '__main__':
+    unittest.main()
