8840cd4d724cb1895c23640b3bae57812f771a44
Rami ALBASHA <ralbasha@msfourrager.com>
2024-11-26 14:44:33 -0500

    calc soil layer depth from slc data

--- src/holos_service/django_stuff.py
+++ src/holos_service/django_stuff.py
@@ -1,4 +1,7 @@
+from pathlib import Path
+
 from geojson import load, FeatureCollection
+from pandas import read_csv, DataFrame
 from shapely.geometry import shape, Point
 
 from holos_service.config import PATH_SLC_GEOJSON_FILE
@@ -25,3 +28,41 @@ def identify_slc_polygon_id(
             return feature['properties']['POLY_ID']
 
     return None
+
+
+def get_dominant_component_soil_id(
+        id_polygon: int,
+        path_slc_cmp: Path
+) -> str:
+    df = read_csv(path_slc_cmp, decimal='.', sep=',', usecols=['POLY_ID', 'PERCENT_', 'SOIL_ID'])
+    return df[df['POLY_ID'] == id_polygon].sort_values(by='PERCENT_', ascending=False).iloc[0]['SOIL_ID']
+
+
+def get_soil_layer_table(
+        path_slc_slt: Path,
+        id_soil: str
+) -> DataFrame:
+    df = read_csv(path_slc_slt, decimal='.', sep=',', usecols=["SOIL_ID", "UDEPTH", "LDEPTH"])
+    df = df[df['SOIL_ID'] == id_soil]
+    return df.sort_values(by='UDEPTH', ascending=True)
+
+
+def get_first_non_litter_layer(
+        soil_layer_table: DataFrame
+) -> dict:
+    return soil_layer_table[soil_layer_table['UDEPTH'] >= 0].iloc[0].to_dict()
+
+
+def get_top_layer_depth(
+        first_non_litter_layer: dict
+) -> int:
+    """
+
+    Args:
+        first_non_litter_layer: first soil layer which is not a litter
+
+    Returns:
+        (mm) depth of the upper first non-litter layer
+
+    """
+    return first_non_litter_layer['LDEPTH'] * 10
