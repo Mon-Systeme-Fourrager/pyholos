99ef2e7fef45335c55206eb5b6646bd789a78cfa
Rami ALBASHA <ralbasha@msfourrager.com>
2024-12-12 17:59:44 -0500

    add beef GroupTypes

--- src/holos_service/components/animals/beef.py
+++ src/holos_service/components/animals/beef.py
@@ -1,4 +1,5 @@
 from holos_service.common import EnumGeneric
+from holos_service.components.animals.common import (AnimalType)
 
 
 class GroupNames(EnumGeneric):
@@ -6,3 +7,19 @@ class GroupNames(EnumGeneric):
     replacement_heifers: str = "Replacement heifers"
     cows: str = "Cows"
     calves: str = "Calves"
+
+
+class GroupTypes(EnumGeneric):
+    beef: str = AnimalType.beef.value  # "Beef"
+    beef_backgrounder: str = AnimalType.beef_backgrounder.value  # "BeefBackgrounder"
+    beef_backgrounder_heifer: str = AnimalType.beef_backgrounder_heifer.value  # "BeefBackgrounderHeifer"
+    beef_backgrounder_steer: str = AnimalType.beef_backgrounder_steer.value  # "BeefBackgrounderSteer"
+    beef_bulls: str = AnimalType.beef_bulls.value  # "BeefBulls"
+    beef_calf: str = AnimalType.beef_calf.value  # "BeefCalf"
+    beef_cow: str = AnimalType.beef_cow.value  # "BeefCow"
+    beef_cow_dry: str = AnimalType.beef_cow_dry.value  # "BeefCowDry"
+    beef_cow_lactating: str = AnimalType.beef_cow_lactating.value  # "BeefCowLactating"
+    beef_finisher: str = AnimalType.beef_finisher.value  # "BeefFinisher"
+    beef_finishing_heifer: str = AnimalType.beef_finishing_heifer.value  # "BeefFinishingHeifer"
+    beef_finishing_steer: str = AnimalType.beef_finishing_steer.value  # "BeefFinishingSteer"
+    beef_replacement_heifers: str = AnimalType.beef_replacement_heifers.value  # "BeefReplacementHeifers"
