8cd284da666cbebecb4afabb945492d68005ae14
Rami ALBASHA <ralbasha@msfourrager.com>
2024-12-13 10:09:24 -0500

    add HolosVar class

--- src/holos_service/common.py
+++ src/holos_service/common.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from typing import Any
 
 
 class EnumGeneric(Enum):
@@ -9,3 +10,21 @@ class EnumGeneric(Enum):
     @classmethod
     def get_member(cls, name: str):
         return getattr(cls, name)
+
+
+class HolosVar:
+    def __init__(
+            self,
+            name: str,
+            value: Any
+    ):
+        self.name = name
+        self.value = value
+
+
+class Component:
+    def __init__(self):
+        pass
+
+    def to_dict(self) -> dict:
+        return {k: v.value for k, v in self.__dict__.items() if isinstance(v, HolosVar)}
