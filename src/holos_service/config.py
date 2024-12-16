d366ea57c035a803f73fe322d12198ffdb391ac7
Rami ALBASHA <ralbasha@msfourrager.com>
2024-12-16 11:05:08 -0500

    refacto config.py

--- src/holos_service/config.py
+++ src/holos_service/config.py
@@ -4,21 +4,23 @@ from pathlib import Path
 
 PATH_HOLOS_CLI = Path(environ['PATH_HOLOS_CLI'])
 
+_PATH_HOLOS_SERVICE_RESOURCES = Path(__file__).parent / 'resources'
+
 
 class PathsHolosResources:
-    path_root = Path(__file__).parent / 'resources/holos'
-    Table_21_Average_Milk_Production_For_Dairy_Cows_By_Province = path_root / (
-        'Table_21_Average_Milk_Production_For_Dairy_Cows_By_Province.csv')
-    Table_16_Livestock_Coefficients_BeefAndDairy_Cattle_Provider = path_root / (
+    _path_root = _PATH_HOLOS_SERVICE_RESOURCES / 'holos'
+    Table_16_Livestock_Coefficients_BeefAndDairy_Cattle_Provider = _path_root / (
         'Table_16_Livestock_Coefficients_BeefAndDairy_Cattle_Provider.csv')
-    Table_30_Default_Bedding_Material_Composition_Provider = path_root / (
+    Table_21_Average_Milk_Production_For_Dairy_Cows_By_Province = _path_root / (
+        'Table_21_Average_Milk_Production_For_Dairy_Cows_By_Province.csv')
+    Table_30_Default_Bedding_Material_Composition_Provider = _path_root / (
         'Table_30_Default_Bedding_Material_Composition_Provider.csv')
 
 
-class PathsSlcData:
-    path_root = _PATH_HOLOS_SERVICE_RESOURCES / 'soil_landscapes_of_canada_v3r2'
-    geojson_file = path_root / 'soil_landscapes_of_canada_v3r2.geojson'
-    csv_dir = path_root / 'soil_landscapes_of_canada_v3r2_csv'
+class PathsSlcData(Enum):
+    _path_root = _PATH_HOLOS_SERVICE_RESOURCES / 'soil_landscapes_of_canada_v3r2'
+    geojson_file = _path_root / 'soil_landscapes_of_canada_v3r2.geojson'
+    csv_dir = _path_root / 'soil_landscapes_of_canada_v3r2_csv'
     cmp_file = csv_dir / 'ca_all_slc_v3r2_cmp.csv'
     slt_file = csv_dir / 'ca_all_slc_v3r2_slt.csv'
     snt_file = csv_dir / 'ca_all_slc_v3r2_snt.csv'
--- tests/test_config.py
+++ tests/test_config.py
@@ -5,15 +5,11 @@ from holos_service import config
 
 class TestPaths(unittest.TestCase):
     def test_paths_exist(self):
-        self.assertTrue(config.PATH_HOLOS_CLI)
+        self.assertIsNotNone(config.PATH_HOLOS_CLI)
 
         for pth in config.PathsSlcData:
-            self.assertTrue(pth.value.exists(), msg=self._set_error_message(pth.value))
-
-    @staticmethod
-    def _set_error_message(s: str):
-        return f'The following file is missing:\n{s}'
-
+            print(pth.value, pth.value.exists())
+            self.assertTrue(pth.value.exists())
 
 if __name__ == '__main__':
     unittest.main()
