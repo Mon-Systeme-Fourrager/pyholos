7b345a8f65eba898abe463825597b1c1e2ada367
Rami ALBASHA <ralbasha@msfourrager.com>
2025-01-08 13:50:28 -0500

    refacto

--- src/holos_service/common.py
+++ src/holos_service/common.py
@@ -1,4 +1,4 @@
-from enum import Enum
+from enum import Enum, unique, StrEnum, auto
 from typing import Any
 
 from holos_service import django_stuff
@@ -54,3 +54,78 @@ def get_region(
         res = Region.EasternCanada
 
     return res
+
+
+@unique
+class ClimateZones(StrEnum):
+    CoolTemperateMoist = auto()
+    CoolTemperateDry = auto()
+    BorealMoist = auto()
+    BorealDry = auto()
+    WarmTemperateMoist = auto()
+    WarmTemperateDry = auto()
+
+
+def get_climate_zone(
+        mean_annual_temperature: float,
+        mean_annual_precipitation: float,
+        mean_annual_potential_evapotranspiration: float
+) -> ClimateZones:
+    """Returns a ClimateZones member for the specified climate conditions
+
+    Args:
+        mean_annual_temperature: (°C) mean annual air temperature
+        mean_annual_precipitation: (mm) mean annual precipitation
+        mean_annual_potential_evapotranspiration: (mm) mean annual potential evapotranspiration
+
+    Returns:
+        ClimateZones object
+
+    Notes:
+        For the determination of the methane conversion factor) MCF value, IPCC (2019) defines the different climate zones as follows:
+            1. Warm temperate moist: mean annual temperature (MAT) > 10 °C, P:PE >1;
+            2. Warm temperate dry: MAT >10 °C, P:PE < 1;
+            3. Cool temperate moist: MAT > 0 °C, P:PE >1;
+            4. Cool temperate dry: MAT > 0 °C, P:PE <1;
+            5. Boreal moist: MAT < 0 °C but some monthly temperatures > 10 °C, P:PE >1;
+            6. Boreal dry: MAT < 0 °C but some monthly temperatures > 10 °C, P:PE <1.
+        The MAT for cool temperate moist, cool temperate dry, warm temperate moist and warm temperate dry were 4.6, 5.8, 13.9, 14.0, respectively.
+        For deep pit manure storage systems for dairy cattle and swine, an average storage duration of 1 month was assumed.
+        (Source: IPCC (2019), Table 10.17)
+
+    Holos Source Code:
+        https://github.com/holos-aafc/Holos/blob/396f1ab9bc7247e6d78766f9445c14d2eb7c0d9d/H.Core/Providers/Animals/Table_37_MCF_By_Climate_Livestock_MansureSystem_Provider.cs#L147
+    """
+    is_high_ratio_precipitation_to_potential_evapotranspiration = (
+            (mean_annual_precipitation / mean_annual_potential_evapotranspiration) >= 1)
+
+    if (mean_annual_temperature >= 10) and is_high_ratio_precipitation_to_potential_evapotranspiration:
+        climate_zone = ClimateZones.WarmTemperateMoist
+
+    elif (mean_annual_temperature >= 10) and not is_high_ratio_precipitation_to_potential_evapotranspiration:
+        climate_zone = ClimateZones.WarmTemperateDry
+
+    elif all([
+        mean_annual_temperature >= 0,
+        mean_annual_temperature < 10,
+        is_high_ratio_precipitation_to_potential_evapotranspiration
+    ]):
+        climate_zone = ClimateZones.CoolTemperateMoist
+
+    elif all([
+        mean_annual_temperature >= 0,
+        mean_annual_temperature < 10,
+        not is_high_ratio_precipitation_to_potential_evapotranspiration
+    ]):
+        climate_zone = ClimateZones.CoolTemperateDry
+
+    elif (mean_annual_temperature <= 0) and is_high_ratio_precipitation_to_potential_evapotranspiration:
+        climate_zone = ClimateZones.WarmTemperateMoist
+
+    elif (mean_annual_temperature <= 0) and not is_high_ratio_precipitation_to_potential_evapotranspiration:
+        climate_zone = ClimateZones.WarmTemperateDry
+
+    else:
+        raise ValueError("Unable to get data for methane conversion factor since climate zone is unknown")
+
+    return climate_zone
