9ead43fb11e520f061ffa965f56150faf29e9f1f
Rami ALBASHA <ralbasha@msfourrager.com>
2025-01-08 15:30:35 -0500

    add new methods to MonthlyWeather

--- src/holos_service/params.py
+++ src/holos_service/params.py
@@ -285,13 +285,28 @@ class MonthlyWeather(ParamGeneric):
             title: str,
             variable_name: str,
             variable_monthly_values: list[int | float],
+            months_of_growing_season: list[str] = ("may", "june", "july", "august", "september", "october"),
             **kwargs
     ):
         super().__init__(title=title)
-        _months = ["January", "February", "March", "April", "May", "June",
-                   "July", "August", "September", "October", "November", "December"]
-        for month, value in zip(_months, variable_monthly_values):
-            setattr(self, month, FarmSettingsVar(name=f'{month} {variable_name}', value=value))
+
+        self.months_of_growing_season = months_of_growing_season
+        self._months = ["january", "february", "march", "april", "may", "june",
+                        "july", "august", "september", "october", "november", "december"]
+        for month, value in zip(self._months, variable_monthly_values):
+            setattr(self, month, FarmSettingsVar(name=f'{month.capitalize()} {variable_name}', value=value))
+
+    def calc_seasonal_sum(self) -> float:
+        return sum([getattr(self, month).value for month in self.months_of_growing_season])
+
+    def calc_seasonal_average(self) -> float:
+        return self.calc_seasonal_sum() / len(self.months_of_growing_season)
+
+    def calc_annual_sum(self) -> float:
+        return sum([getattr(self, month).value for month in self._months])
+
+    def calc_annual_average(self) -> float:
+        return sum([getattr(self, month).value for month in self._months]) / 12.
 
 
 class ParamsSoil(ParamGeneric):
