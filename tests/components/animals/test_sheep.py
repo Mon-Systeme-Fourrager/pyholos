beee1a4c7b6a1dda9fec7ce832a01564c0c7d028
Rami Albasha <rami.albacha@yahoo.com>
2025-01-08 16:12:30 -0500

    Update test_sheep.py

--- tests/components/animals/test_sheep.py
+++ tests/components/animals/test_sheep.py
@@ -28,7 +28,7 @@ class TestGetAnimalCoefficientData(unittest.TestCase):
         print(cls.animal_coefficients)
 
     def setUp(self):
-        self.sheep = sheep.SheepBase()
+        self.sheep = sheep.Sheep()
 
     def test_get_animal_coefficient_data_for_sheep_feedlot(self):
         self.sheep.group_name.value = sheep.GroupNames.sheep_feedlot.value
