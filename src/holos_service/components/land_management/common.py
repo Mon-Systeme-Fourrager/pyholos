ed75ad5930d4c44683ca7baf531836eefbf51bc9
Rami ALBASHA <ralbasha@msfourrager.com>
2025-01-15 09:47:45 -0500

    add TillageType

--- src/holos_service/components/land_management/common.py
+++ src/holos_service/components/land_management/common.py
@@ -3,6 +3,17 @@ from enum import auto
 from holos_service.utils import AutoNameEnum, concat_lists
 
 
+class TillageType(AutoNameEnum):
+    """
+    Holos Source Code:
+        https://github.com/holos-aafc/Holos/blob/b183dab99d211158d1fed9da5370ce599ac7c914/H.Core/Enumerations/TillageType.cs#L6
+    """
+    NotSelected = auto()
+    Reduced = auto()
+    NoTill = auto()
+    Intensive = auto()
+
+
 class CropType(AutoNameEnum):
     """
     Holos Source Code:
