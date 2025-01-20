035fae20849da65ed306da8f62241ce4eb5d1bff
Rami ALBASHA <ralbasha@msfourrager.com>
2025-01-20 10:21:27 -0500

    add NitrogenDepositionAmount to CoreConstants

--- src/holos_service/core_constants.py
+++ src/holos_service/core_constants.py
@@ -1,2 +1,6 @@
 class CoreConstants:
     NotApplicable = -100000
+    NitrogenDepositionAmount = 5
+    """(kg(N)/ha/year) Atmospheric Nitrogen deposition amount
+    Holos source doc: page 74 in https://github.com/holos-aafc/Holos/raw/refs/heads/main/AAFC_Technical_Report_Holos_V4.0_Algorithm_Document_DRAFTVersion_18Nov2024.docx
+    """
