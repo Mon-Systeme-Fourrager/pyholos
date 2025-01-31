86aa4f50392b888f94609e7baf0042f0278ffa6b
Rami ALBASHA <ralbasha@msfourrager.com>
2025-01-31 12:40:11 -0500

    add div variables to Defaults

--- src/holos_service/defaults.py
+++ src/holos_service/defaults.py
@@ -23,5 +23,39 @@ class Defaults:
     PercentageOfStrawReturnedToSoilForRootCrops = 100
     """(%) percentage of the root crops straw biomass returned to soil after harvest"""
 
+    # for annual crops
+    EmergenceDay = 141
+    """(julian day) day of plant emergence for annual crops"""
 
+    RipeningDay = 197
+    """(julian day) day of plant ripening for annual crops"""
+
+    Variance = 300
+    """width of distribution function for annual crops"""
+
+    # for perennial crops
+    EmergenceDayForPerennials = 75
+    """(julian day) day of plant emergence for perennial crops"""
+
+    RipeningDayForPerennials = 300
+    """(julian day) day of plant ripening for perennial crops"""
+
+    VarianceForPerennials = 1500
+    """width of distribution function for perennial crops"""
+
+    # for all crops
+    Alfa = 0.7
+    """(-) minimum water storage fraction of wilting_point"""
+
+    DecompositionMinimumTemperature = -3.78
+    """(degree Celsius) maximum cardinal temperature for decomposition"""
+
+    DecompositionMaximumTemperature = 30
+    """(degree Celsius) minimum cardinal temperature for decomposition"""
+
+    MoistureResponseFunctionAtWiltingPoint = 0.18
+    """(mm3/mm3) soil volumetric water content at reference wilting point"""
+
+    MoistureResponseFunctionAtSaturation = 0.42
+    """(mm3/mm3) soil volumetric water content at reference saturation"""
 
