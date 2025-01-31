ae094358c83675e32d211de7f12af11b549e3faf
Rami ALBASHA <ralbasha@msfourrager.com>
2025-01-31 12:12:41 -0500

    add calculate_management_factor()

--- src/holos_service/core_constants.py
+++ src/holos_service/core_constants.py
@@ -1,5 +1,8 @@
 class CoreConstants:
+    DefaultNumberOfDecimalPlaces: int = 3
+
     NotApplicable = -100000
+
     NitrogenDepositionAmount = 5
     """(kg(N)/ha/year) Atmospheric Nitrogen deposition amount
     Holos source doc: page 74 in https://github.com/holos-aafc/Holos/raw/refs/heads/main/AAFC_Technical_Report_Holos_V4.0_Algorithm_Document_DRAFTVersion_18Nov2024.docx
