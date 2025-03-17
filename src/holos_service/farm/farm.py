a00a2f8a8f6ac91d4b90d4a4b6d1d40b86903793
Rami ALBASHA <ralbasha@msfourrager.com>
2025-03-17 15:30:54 -0400

    update create_farm()

--- src/holos_service/farm/farm.py
+++ src/holos_service/farm/farm.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
 from holos_service.django_stuff import CanadianProvince
-from holos_service.farm.farm_inputs import BeefCattleInput, DairyCattleInput, SheepFlockInput
+from holos_service.farm.farm_inputs import BeefCattleInput, DairyCattleInput, SheepFlockInput, FarmGeneralInfo
 from holos_service.soil import SoilTexture
 
 
@@ -23,8 +23,7 @@ def write_animal_input_csv(
 
 
 def create_farm(
-        province: CanadianProvince,
-        soil_texture: SoilTexture,
+        farm_general_info: FarmGeneralInfo,
         path_dir_farm: Path,
         beef_cattle_data: BeefCattleInput = None,
         dairy_cattle_data: DairyCattleInput = None,
@@ -32,19 +31,20 @@ def create_farm(
         field_data=None,
 ) -> None:
     path_dir_farm.mkdir(parents=True, exist_ok=True)
+    farm_general_info.set_farm_settings().write(path_dir_farm=path_dir_farm)
 
     for animal_data, dir_name in [
         (beef_cattle_data, 'Beef'),
         (dairy_cattle_data, 'Dairy'),
         (sheep_flock_data, 'Sheep')]:
         if animal_data is not None:
-            path_dir_animal=path_dir_farm / dir_name
+            path_dir_animal = path_dir_farm / dir_name
             path_dir_animal.mkdir(parents=True, exist_ok=True)
 
             write_animal_input_csv(
                 animal_data=animal_data,
-                province=province,
-                soil_texture=soil_texture,
+                province=farm_general_info.province,
+                soil_texture=farm_general_info.soil_texture,
                 path_dir_animal=path_dir_farm / dir_name
             )
 
