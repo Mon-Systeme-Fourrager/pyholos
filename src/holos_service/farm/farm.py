b18bdbf8656931bbb4ebdc8d6e1f2beecfa0de38
Rami ALBASHA <ralbasha@msfourrager.com>
2025-03-19 10:51:03 -0400

    create fields input csvs

--- src/holos_service/farm/farm.py
+++ src/holos_service/farm/farm.py
@@ -1,8 +1,11 @@
 from pathlib import Path
 
+from pandas import DataFrame
+
 from holos_service.components.common import convert_province_name
 from holos_service.django_stuff import CanadianProvince
-from holos_service.farm.farm_inputs import (BeefCattleInput, DairyCattleInput, SheepFlockInput, WeatherSummary)
+from holos_service.farm.farm_inputs import (BeefCattleInput, DairyCattleInput, SheepFlockInput, WeatherSummary,
+                                            FieldsInput)
 from holos_service.farm.farm_settings import ParamsFarmSettings
 from holos_service.soil import (SoilTexture, convert_soil_texture_name, convert_soil_functional_category_name,
                                 SoilFunctionalCategory)
@@ -31,9 +34,27 @@ def write_animal_input_csv(
         path_dir=path_dir_animal
     )
 
-        df.to_csv(path_dir_animal / f'{name_output_file}.csv', index=False)
 
-    pass
+def create_fields_input_csv(
+        fields_data: FieldsInput,
+        province: CanadianProvince,
+        clay_content: float,
+        sand_content: float,
+        organic_carbon_percentage: float,
+        soil_top_layer_thickness: float,
+        soil_functional_category: SoilFunctionalCategory,
+        path_dir_fields: Path
+) -> None:
+    write_input_csv(
+        dfs=fields_data.create_components(
+            province=province,
+            clay_content=clay_content,
+            sand_content=sand_content,
+            organic_carbon_percentage=organic_carbon_percentage,
+            soil_top_layer_thickness=soil_top_layer_thickness,
+            soil_functional_category=soil_functional_category),
+        path_dir=path_dir_fields
+    )
 
 
 def create_farm(
@@ -44,7 +65,7 @@ def create_farm(
         beef_cattle_data: BeefCattleInput = None,
         dairy_cattle_data: DairyCattleInput = None,
         sheep_flock_data: SheepFlockInput = None,
-        field_data=None,
+        fields_data: FieldsInput = None,
 ) -> None:
     path_dir_farm.mkdir(parents=True, exist_ok=True)
     farm_settings = ParamsFarmSettings(
@@ -74,7 +95,21 @@ def create_farm(
                 animal_data=animal_data,
                 province=province,
                 soil_texture=soil_texture,
-                path_dir_animal=path_dir_farm / dir_name
-            )
+                path_dir_animal=path_dir_farm / dir_name)
+
+    if fields_data is not None:
+        path_dir_fields = path_dir_farm / 'Fields'
+        path_dir_fields.mkdir(parents=True, exist_ok=True)
+
+        create_fields_input_csv(
+            fields_data=fields_data,
+            path_dir_fields=path_dir_fields,
+            province=province,
+            clay_content=params_soil.proportion_of_clay_in_soil.value,
+            sand_content=params_soil.proportion_of_sand_in_soil.value,
+            organic_carbon_percentage=params_soil.proportion_of_soil_organic_carbon.value,
+            soil_top_layer_thickness=params_soil.top_layer_thickness.value,
+            soil_functional_category=convert_soil_functional_category_name(params_soil.soil_functional_category.value),
+        )
 
     pass
