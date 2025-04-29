5d27c00f319adce5e83eda8be93506eda95c11c2
Rami ALBASHA <ralbasha@msfourrager.com>
2025-04-29 14:54:30 -0400

    read farm monthly co2 data

--- src/pyholos/postprocessing/postprocessing.py
+++ src/pyholos/postprocessing/postprocessing.py
@@ -15,3 +15,24 @@ def read_equivalent_co2_emissions(
     df = df[~df['Farm Name'].str.contains('All Farms')]
 
     return df
+
+
+def read_farm_monthly_equivalent_co2_emissions(
+        path_outputs: Path,
+        year: int
+) -> DataFrame:
+    df = read_csv(path_outputs, decimal='.', sep=',').rename(columns=lambda x: x.strip())
+    df = df[[s for s in df.columns if "Unnamed" not in s]]
+    fill_cols = ['Farm Name', 'Component Category', 'Component Name', 'Group Name']
+    df.loc[:, fill_cols] = df.loc[:, fill_cols].ffill()
+    df = df.dropna(axis=0)
+    df.loc[:, 'Farm Name'] = df['Farm Name'].apply(lambda x: x.replace('_Farm', '')).to_list()
+    df.loc[:, 'Year'] = df['Year'].astype(str)
+
+    df = df[
+        (~df['Year'].str.contains('Totals|Total')) &
+        (df['Year'] == str(year))
+        ]
+    df = df[~df['Farm Name'].str.contains('All Farms')]
+
+    return df.drop(columns=['Year'])
